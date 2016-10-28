f37f263c23475235c211d150e9c32e5d367a895d
libing <348802969@qq.com>
2016-10-28 16:46:56 +0800

    test

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 var http = require('http');
+var child_process = require("child_process");
 http.createServer(function(req, res) {
-            var post_data = '';
-            req.on('data', function(chunk) {
-                post_data += chunk;
-            });
-            req.on('end', function() {
-                    console.log(post_data);
-                });
-                res.writeHead(200, { 'Content-Type': 'text/plain' }); res.end('hello here!');
-            }).listen(8888); console.log('listen 8888!');
+    var post_data = '';
+    req.on('data', function(chunk) {
+        post_data += chunk;
+    });
+    req.on('end', function() {
+        child_process.execSync('git pull origin master');
+    });
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('get push post!');
+}).listen(8888);
