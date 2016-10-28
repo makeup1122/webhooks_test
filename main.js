e4c6c92e641c15e3dc1b673df823378f3f20e4cb
libing <348802969@qq.com>
2016-10-28 16:20:00 +0800

    test2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function(req, res) {
-    console.log(JSON.parse(req.body));
+    console.log(req.body);
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('hello here!');
-}).listen(8888); 
+}).listen(8888);
 console.log('listen 8888!');
